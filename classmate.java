060c2c7a1a2a77ebc792ab15ad3051372675b838
alsabhst <salsabila_raharjo_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:34:14 +0700

    Add files via upload

---
+++ classmate.java
@@ -0,0 +1,40 @@
+public class classmate {
+    public static void main(String[] args) {
+        String nama = "Faliha Afra";
+        int usia = 15;
+        String cita2 = "Traveler";   
+        System.out.println(nama);
+        System.out.println(usia + " tahun");
+        System.out.println(cita2);
+
+        String nama2 = "Shabrina";
+        int usia2 = 16;
+        String cita22 = "Bos sukses";   
+        System.out.println(nama2);
+        System.out.println(usia2 + " tahun");
+        System.out.println(cita22);
+
+        String nama3 = "Tazkia Adila";
+        int usia3 = 15;
+        String cita23 = "Graphic Designer";   
+        System.out.println(nama3);
+        System.out.println(usia3 + " tahun");
+        System.out.println(cita23);
+
+        String nama4 = "Zian Mahdia";
+        int usia4 = 15;
+        String cita24 = "Pengusaha";   
+        System.out.println(nama4);
+        System.out.println(usia4 + " tahun");
+        System.out.println(cita24);
+
+        String nama5 = "Azka Rullah";
+        int usia5 = 15;
+        String cita25 = "Web Developer";   
+        System.out.println(nama5);
+        System.out.println(usia5 + " tahun");
+        System.out.println(cita25);
+
+         
+         }
+        }
