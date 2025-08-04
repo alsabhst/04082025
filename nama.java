271db89f3333302a62b97450059a352e6a8603a9
alsabhst <salsabila_raharjo_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:50 +0700

    Add files via upload

---
+++ nama.java
@@ -0,0 +1,14 @@
+public class nama {
+    public static void main(String[] args) {
+        //main method
+         System.out.println("Salsabila Abhista");
+         System.out.println("SMK Telkom Malang");
+         System.out.println("SMP Bintang Madani");
+         System.out.println("Eko Slamet");
+         System.out.println("Maya Veronika");
+         System.out.println("Pengusaha");
+         System.out.println("Mendengarkan musik"););
+
+}
+    
+}
